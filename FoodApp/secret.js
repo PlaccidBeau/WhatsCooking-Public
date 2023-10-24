c44ac9cbab9d55ea456797030baf2beba47afc05
Eric Beauford <ericbeauford@gmail.com>
2023-10-24 14:30:34 -0500

    Post to public

---
+++ FoodApp/secret.js
@@ -0,0 +1,9 @@
+//DO NOT POST ME ON GITHUB
+module.exports.DB_URL = "";
+//mongodb connection string
+module.exports.CLOUDINARY_CLOUD_NAME = "";
+// Enter CLOUDINARY name
+module.exports.CLOUDINARY_KEY = "";
+// Enter CLOUDINARY key
+module.exports.ClOUDINARY_SECRET = "";
+// Enter CLOUDINARY secret
